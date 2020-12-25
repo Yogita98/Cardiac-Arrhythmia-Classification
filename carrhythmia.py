c95125576646269e11c098b6f420a2dac5510d5b
Yogita Bhatia <yogitab2798@gmail.com>
2020-12-25 16:41:51 +0530

    Changed model paths

--- carrhythmia.py
+++ carrhythmia.py
@@ -46,7 +46,7 @@ print('Running on http://localhost:5000')
 # import os
 base_path = os.path.dirname(os.path.abspath(__file__))
 # filename = os.path.join(base_path, 'xception_fine_tuned.h5')
-filename = 'flaskModel.h5'
+filename = 'C:/Users/bhati/Desktop/My docs/Hosted BE Project/Cardiac-Arrhythmia-Classification/app/flaskModel.h5'
 model = load_model(filename)
 print('Xception Model loaded.')
 
